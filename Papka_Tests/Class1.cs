d05ffa35aebf217672adffa23e86e22b37c5f8f6
KazGad <90814274+KazGad@users.noreply.github.com>
2021-09-17 08:36:00 +0300

    Add files via upload

---
+++ Papka_Tests/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        static void Main(string[] args)
+        {
+            bool arr = Class1.validatePassword("");
+            Console.WriteLine("res = " + arr);
+
+            string password = "ABVGD";
+            Console.WriteLine("len = " + password.Length);
+
+            Console.ReadKey();
+        }
+
+        public static bool validatePassword(string password)
+        {
+            if (password.Length < 8 || password.Length > 20)
+                return false;
+            if (!password.Any(Char.IsLower))
+                return false;
+            if (!password.Any(Char.IsUpper))
+                return false;
+            if (!password.Any(Char.IsDigit))
+                return false;
+            if (password.Intersect("#$%^&_").Count() == 0)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Papka_Tests/Class1Tests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void validatePasswordTest_Symbols_T1()
+        {
+            string password = "Asd123$";
+            bool actual = Class1.validatePassword(password);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_Symbols_T2()
+        {
+            string password = "ASDqwe123$";
+            bool expected = true;
+            bool actual = Class1.validatePassword(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_Symbols_T3()
+        {
+            string password = "ZXCaSdqwe1234567890$%";
+            bool actual = Class1.validatePassword(password);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_Num_T1()
+        {
+            string password = "ZXCaSdqwe$";
+            bool actual = Class1.validatePassword(password);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_Num_T2()
+        {
+            string password = "ASDqwe123$";
+            bool expected = true;
+            bool actual = Class1.validatePassword(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_SpecNum_T1()
+        {
+            string password = "ASDqwe123";
+            bool actual = Class1.validatePassword(password);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_SpecNum_T2()
+        {
+            string password = "ASDqwe123$";
+            bool expected = true;
+            bool actual = Class1.validatePassword(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_Caps_T1()
+        {
+            string password = "asdqwe123$";
+            bool actual = Class1.validatePassword(password);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_Caps_T2()
+        {
+            string password = "ASDqwe123$";
+            bool expected = true;
+            bool actual = Class1.validatePassword(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_Low_T1()
+        {
+            string password = "ASDQWE123$";
+            bool actual = Class1.validatePassword(password);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void validatePasswordTest_Low_T2()
+        {
+            string password = "ASDqwe123$";
+            bool expected = true;
+            bool actual = Class1.validatePassword(password);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
