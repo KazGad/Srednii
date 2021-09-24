0e314e8a68b4f4396ada81e5bd9886357fa64937
KazGad <90814274+KazGad@users.noreply.github.com>
2021-09-24 08:53:39 +0300

    Add files via upload

---
+++ Cuculator/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuculator
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите перове число:");
+            int n1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int n2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите действие:");
+            String Sym = Console.ReadLine();
+            int arr = Class1.validateSym(n1, n2, Sym);
+            Console.WriteLine("resout = " + arr);
+
+            Console.ReadKey();
+        }
+        public static int validateSym(int n1, int n2, String Sym)
+        {
+            if (Sym == "+")
+                return n1 + n2;
+            if (Sym == "-")
+                return n1 - n2;
+            if (Sym == "*")
+                return n1 * n2;
+            if (Sym == "/")
+            {
+                try
+                {
+                    if (n2 == 0)
+                    {
+                        throw new DivideByZeroException("Не делите на ноль!");
+                    }
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine($"Ошибка: {e.Message}");
+                }
+                Console.Read();
+                /*if (n2 == 0)
+                {
+                    Console.WriteLine(0);
+                }*/
+                return n1 / n2;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Cuculator/Class1Tests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cuculator.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void validateSymTest_plus()
+        {
+            string Sym = "+";
+            int resout = 12;
+            int n1 = 5;
+            int n2 = 7;
+            int actual = Class1.validateSym(n1, n2, Sym);
+            Assert.AreEqual(resout, actual);
+        }
+
+        [TestMethod()]
+        public void validateSymTest_minus()
+        {
+            string Sym = "-";
+            int resout = 2;
+            int n1 = 7;
+            int n2 = 5;
+            int actual = Class1.validateSym(n1, n2, Sym);
+            Assert.AreEqual(resout, actual);
+        }
+
+        [TestMethod()]
+        public void validateSymTest_multiplication()
+        {
+            string Sym = "*";
+            int resout = 35;
+            int n1 = 7;
+            int n2 = 5;
+            int actual = Class1.validateSym(n1, n2, Sym);
+            Assert.AreEqual(resout, actual);
+        }
+
+        [TestMethod()]
+        public void validateSymTest_share()
+        {
+            string Sym = "/";
+            int resout = 6;
+            int n1 = 30;
+            int n2 = 5;
+            int actual = Class1.validateSym(n1, n2, Sym);
+            Assert.AreEqual(resout, actual);
+        }
+    }
+}
