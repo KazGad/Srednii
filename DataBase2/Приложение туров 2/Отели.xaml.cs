851901d40ab024b24736d616bafce82a001e399f
Your Name <test@example.com>
2021-10-20 11:11:59 +0300

    Tour

--- DataBase2/Приложение туров 2/Отели.xaml.cs
+++ DataBase2/Приложение туров 2/Отели.xaml.cs
@@ -23,22 +23,47 @@ namespace Приложение_туров_2
         public Отели()
         {
             InitializeComponent();
-            DGridHotels.ItemsSource = SISEntities.GetContext().Hotel.ToList();
+            //DGridHotels.ItemsSource = SISEntities.GetContext().Hotel.ToList();
         }
 
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
-
+            Мэнеджер.MainFrame.Navigate(new Тестирование_и_навигация((sender as Button).DataContext as Hotel)) ;
         }
 
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
-            Мэнеджер.MainFrame.Navigate(new Тестирование_и_навигация());
+            Мэнеджер.MainFrame.Navigate(new Тестирование_и_навигация(null));
         }
 
         private void BtnDel_Click(object sender, RoutedEventArgs e)
         {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие ({hotelsForRemoving.Count()}) элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    SISEntities.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    SISEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
 
+                    DGridHotels.ItemsSource = SISEntities.GetContext().Hotel.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                SISEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = SISEntities.GetContext().Hotel.ToList();
+            }
         }
     }
 }
--- DataBase2/Приложение туров 2/Тестирование и навигация.xaml.cs
+++ DataBase2/Приложение туров 2/Тестирование и навигация.xaml.cs
@@ -20,15 +20,47 @@ namespace Приложение_туров_2
     /// </summary>
     public partial class Тестирование_и_навигация : Page
     {
-        public Тестирование_и_навигация()
+        private Hotel _currentHotel = new Hotel();
+        public Тестирование_и_навигация(Hotel selectedHotel)
         {
             InitializeComponent();
+
+            if (selectedHotel != null)
+                _currentHotel = selectedHotel;
+
+            DataContext = _currentHotel;
             ComboCountries.ItemsSource = SISEntities.GetContext().Country.ToList();
         }
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
+            StringBuilder errors = new StringBuilder();
 
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                errors.AppendLine("Укажите название отеля");
+            if (_currentHotel.CountOfStars < 1 || _currentHotel.CountOfStars > 5)
+                errors.AppendLine("Количество звоёзд - число от 1 до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберите страну");
+
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentHotel.Id == 0)
+                SISEntities.GetContext().Hotel.Add(_currentHotel);
+
+            try
+            {
+                SISEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                Мэнеджер.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
