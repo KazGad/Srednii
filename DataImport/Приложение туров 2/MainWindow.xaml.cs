d7bff95e34db9228055379e94ac65c79e7c45ebb
Your Name <test@example.com>
2021-10-22 11:34:58 +0300

    Tour

---
+++ DataImport/Приложение туров 2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Приложение_туров_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Страница_Отелей());
+            Мэнеджер.MainFrame = MainFrame;
+
+            ImportTours();
+        }
+
+        private void ImportTours()
+        {
+            var fileData = File.ReadAllLines(@"C:\Users\Администратор\source\repos\AIOT.txt");
+            var image = Directory.GetFiles(@"C:\Users\Администратор\source\repos\Новая папка");
+
+            foreach (var line in fileData)
+            {
+                var data = line.Split('\t');
+
+                var tempTour = new Tour
+                {
+                    Name = data[0].Replace("\"", ""),
+                }
+            }
+        }
+
+        private void BtnBack_Ckick(object sender, RoutedEventArgs e)
+        {
+            Мэнеджер.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+
+            else
+                BtnBack.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ DataImport/Приложение туров 2/Отели.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Приложение_туров_2
+{
+    /// <summary>
+    /// Логика взаимодействия для Отели.xaml
+    /// </summary>
+    public partial class Отели : Page
+    {
+        public Отели()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Мэнеджер.MainFrame.Navigate(new Тестирование_и_навигация((sender as Button).DataContext as Hotel));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Мэнеджер.MainFrame.Navigate(new Тестирование_и_навигация(null));
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие ({hotelsForRemoving.Count()}) элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    SISEntities.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    SISEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+
+                    DGridHotels.ItemsSource = SISEntities.GetContext().Hotel.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                SISEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = SISEntities.GetContext().Hotel.ToList();
+            }
+        }
+    }
+}
