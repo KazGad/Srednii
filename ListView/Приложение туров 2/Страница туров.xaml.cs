6a2d6048c1f6e177f225ddf9f0f0dfd6130473e8
Your Name <test@example.com>
2021-11-08 09:04:40 +0300

    Tour

--- ListView/Приложение туров 2/Страница туров.xaml.cs
+++ ListView/Приложение туров 2/Страница туров.xaml.cs
@@ -23,8 +23,48 @@ namespace Приложение_туров_2
         public Страница_туров()
         {
             InitializeComponent();
+
+            var allTypes = SISEntities.GetContext().Type.ToList();
+            allTypes.Insert(0, new Type
+            {
+                Name = "Все типы"
+            });
+            CType.ItemsSource = allTypes;
+
+            CheckActual.IsChecked = true;
+            CType.SelectedIndex = 0;
+
+            UpdateTours();
+        }
+
+        private void UpdateTours()
+        {
             var currentTours = SISEntities.GetContext().Tour.ToList();
-            LViewTour.ItemsSource = currentTours;
+
+            if (CType.SelectedIndex > 0)
+                currentTours = currentTours.Where(p => p.Type.Contains(CType.SelectedItem as Type)).ToList();
+
+            currentTours = currentTours.Where(p => p.Name.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if (CheckActual.IsChecked.Value)
+                currentTours = currentTours.Where(p => p.IsActual).ToList();
+
+            LViewTour.ItemsSource = currentTours.OrderBy(p => p.TicketCount).ToList();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void CType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateTours();
+        }
+
+        private void CActual_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateTours();
         }
     }
 }
