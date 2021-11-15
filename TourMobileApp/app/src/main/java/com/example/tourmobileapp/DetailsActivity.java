d2298294c4f80ca541d6dc2b6ad20c3be8c0c9c9
Your Name <test@example.com>
2021-11-15 10:20:05 +0300

    Tour

--- TourMobileApp/app/src/main/java/com/example/tourmobileapp/DetailsActivity.java
+++ TourMobileApp/app/src/main/java/com/example/tourmobileapp/DetailsActivity.java
@@ -3,6 +3,11 @@ package com.example.tourmobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Hotel currentHotel = getIntent().getParcelableExtra("hotel");
+        ImageView ingPhoto = findViewById(R.id.imageViewDetailsPhoto);
+        TextView txtName = findViewById(R.id.textViewDetailsName);
+        RatingBar rtStars = findViewById(R.id.ratingBarDetailsStars);
+
+        ingPhoto.setImageBitmap(currentHotel.getBitmapSource());
+        txtName.setText(currentHotel.getName());
+        rtStars.setRating(currentHotel.getCountOfStars());
     }
 }
--- TourMobileApp/app/src/main/java/com/example/tourmobileapp/MainActivity.java
+++ TourMobileApp/app/src/main/java/com/example/tourmobileapp/MainActivity.java
@@ -2,13 +2,88 @@ package com.example.tourmobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HotelsAdapter mAdapter;
+    private List<Hotel> mHotelsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lvHotels = findViewById(R.id.listViewHotels);
+        mAdapter = new HotelsAdapter(MainActivity.this, mHotelsList);
+        lvHotels.setAdapter(mAdapter);
+
+        new GetHotels().execute();
     }
+
+    private class GetHotels extends AsyncTask<Void, Void, String>
+    {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try
+            {
+                URL url = new URL("http://localhost:53329/api/Hotels");
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                StringBuilder result = new StringBuilder();
+                String line = "";
+
+                while ((line = reader.readLine()) != null)
+                {
+                    result.append(line);
+                }
+                return result.toString();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try
+            {
+                JSONArray tempArray = new JSONArray(s);
+                for (int i=0; i < tempArray.length(); i++)
+                {
+                    JSONObject hotelJson = tempArray.getJSONObject(i);
+                    Hotel tempHotel = new Hotel(
+                            hotelJson.getInt("Id"),
+                            hotelJson.getString("Name"),
+                            hotelJson.getInt("CountOfStars"),
+                            hotelJson.getString("HotelImage")
+                    );
+                    mHotelsList.add(tempHotel);
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+
 }
